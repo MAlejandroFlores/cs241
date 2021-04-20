be543fd7f82bd5f900f887d45d36f4871b73529b
MAlejandroFlores <alexfloresm@byui.edu>
2021-04-20 11:52:02 -0700

    changes

--- week01/myAssign01.py
+++ week01/myAssign01.py
@@ -2,10 +2,11 @@ import random
 from random import randint
 
 print("Welcome to the number guessing game!")
-# ... other stuff here ...
-seed = input("Enter random seed: \n")
+# Ask for the seed
+seed = input("Enter random seed: ")
+#Create the random number based on the seed
 random.seed(seed)
-# ... possibly more stuff here ...
+# Initiating the loop with the try again value
 try_again = 'yes'
 while try_again != 'no':
     num = randint(1, 50)
@@ -14,14 +15,15 @@ while try_again != 'no':
 
     while num != guess_number:
         guess_tries += 1
-        guess_number = int(input("Please enter a guess: "))
+        # print("\nPlease enter a guess:")
+        guess_number = int(input("\nPlease enter a guess: "))
         if guess_number == num:
             print("Congratulations. You guessed it!")
             print("It took you " + str(guess_tries) + " guesses.")
             break
         else:
             if guess_number > num:
-                print("Lower\n")
+                print("Lower")
             else:
                 print("Higher\n")
     try_again = input("Would you like to play again (yes/no)? ")
