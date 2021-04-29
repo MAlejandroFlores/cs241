6d12faa8b98d2acc0e6d147ae7b1b802c4a11a9c
MAlejandroFlores <alexfloresm@byui.edu>
2021-04-28 21:27:10 -0700

    teamActivity

--- week02/teamact.py
+++ week02/teamact.py
@@ -2,22 +2,33 @@ def prompt_filename():
     file_name = input('Please enter a filename: ')
     return file_name
 
-def main():
-    filename = prompt_filename()
-    print('Opening ' + filename)
-    parse_file(filename)
 
-def parse_file(filename):
-    pride_count = 0
+def parse_file(filename, word_to_find):
+    word_count = 0
     with open(filename) as file:
         file_data = file.read()
         lines = file_data.split('\n')
         for line in lines:
             words = line.split()
             for word in words:
-                if word.lower().strip('.') == 'pride':
-                    pride_count += 1 
-    print('The word pride occurs ' + str(pride_count) + ' times in this file')
+                # if word.lower().strip('.') == 'pride':
+                if word.lower().strip('.') == word_to_find:
+                    word_count += 1 
+
+    return word_count
+    
+
+def prompt_word():
+    word = input('Which word do you want to search: ')
+    return word
+
+
+def main():
+    filename = prompt_filename()
+    print('Opening ' + filename)
+    word_to_find = prompt_word()
+    count = parse_file(filename, word_to_find)
+    print('The word ' + word_to_find + ' occurs ' + str(count) + ' times in this file')
         
 if __name__ == "__main__":
     main()
