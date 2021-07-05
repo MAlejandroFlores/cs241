18b9282f76b04aaeb928e4249b998af78203e759
MAlejandroFlores <alexfloresm@byui.edu>
2021-07-05 12:12:48 -0700

    AssignmentWeek11

---
+++ week11/homework.py
@@ -0,0 +1,13 @@
+
+
+print("Printing list comprension...")
+x = [x for x in range(100)]
+print(x)
+
+print("Number divisibles by 5 and 7")
+y = [y for y in x if (y % 5 == 0 or y % 7 == 0)]
+print(y)
+
+print("Printing words with 4 letters and letter e... ")
+old_words = ["tacos", "knowledge", "water", "on", "the", "I", "is", "hilarious", "tie", "coat", "white", "covenants", "phone", "rubric", "send", "restrictions"]
+new_words = [len for ]
